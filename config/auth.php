8c3bda8d5f29339d4397149fccba8f0bedd072e3
masonturner <msntrnrdev@gmail.com>
2025-07-15 14:23:00 -0400

    Removes the unused user model and associated references

--- config/auth.php
+++ config/auth.php
@@ -59,17 +59,7 @@ return [
     |
     */
 
-    'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
-    ],
+    'providers' => [],
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ArtistController;
-
-Route::get('/users', [UserController::class, 'index']);
 
 Route::get('/artists', [ArtistController::class, 'index']);
 Route::get('/artists/{uuid}', [ArtistController::class, 'show']);
